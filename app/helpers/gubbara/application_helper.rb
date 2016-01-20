9200927ea6eaac9a4ba1158e5c8ce3d0d0b033f2
nitanshu verma <nitanshu@headerlabs.com>
2016-01-20 19:40:17 +0530

    added helpers which finds _path and _url and then convert it to main_app.found_path

--- app/helpers/gubbara/application_helper.rb
+++ app/helpers/gubbara/application_helper.rb
@@ -1,8 +1,35 @@
 module Gubbara
   module ApplicationHelper
+
     def list_notice
       session[:hidden_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?)", session[:hidden_notices]) : @notices = Gubbara::Notice.all
       concat(render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices}))
     end
+#-----------------Handles main_app.root_path problems now you don't need to define application path as main_app.path----------------#
+    def method_missing method, *args, &block
+      puts "LOOKING FOR ROUTES #{method}"
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          main_app.send(method, *args, &block)
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+#------------------------------------------------------------------------------------------------------------------------------------#
   end
 end
