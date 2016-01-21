39476bd7aaff4da8668bc8d207d6e05845578062
nitanshu verma <nitanshu@headerlabs.com>
2016-01-21 18:51:37 +0530

    i changed crlf to false

--- app/helpers/gubbara/application_helper.rb
+++ app/helpers/gubbara/application_helper.rb
@@ -5,9 +5,10 @@ module Gubbara
       session[:hidden_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?)", session[:hidden_notices]) : @notices = Gubbara::Notice.all
       render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices})
     end
+
 #-----------------Handles main_app.root_path problems now you don't need to define application path as main_app.path----------------#
     def method_missing method, *args, &block
-           puts "LOOKING FOR ROUTES #{method}"
+      puts "LOOKING FOR ROUTES #{method}"
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
         if main_app.respond_to?(method)
           main_app.send(method, *args, &block)
