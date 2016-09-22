f07ed1cc67ca9882c062ee2c8c8e39ef328345f1
nitanshu verma <nitanshu1991@gmail.com>
2016-09-22 14:10:21 +0530

    removed the issue of two elements coming while listing notice

--- app/helpers/gubbara/application_helper.rb
+++ app/helpers/gubbara/application_helper.rb
@@ -2,7 +2,7 @@ module Gubbara
   module ApplicationHelper
     def list_notice
       session[:hidden_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?)", session[:hidden_notices]) : @notices = Gubbara::Notice.all
-      concat(render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices}))
+      render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices})
     end
   end
 end
