14c18053896cada9f240008a682545b450927427
nitanshu <nitanshu1991@gmail.com>
2017-03-07 19:10:11 +0530

    basic css and removed unnecessary migration file

--- db/migrate/20151103123233_create_gubbara_notices.rb
+++ db/migrate/20151103123233_create_gubbara_notices.rb
@@ -2,6 +2,7 @@ class CreateGubbaraNotices < ActiveRecord::Migration
   def change
     create_table :gubbara_notices do |t|
       t.string :message
+      t.boolean :active, default: true
 
       t.timestamps
     end
--- db/migrate/20170307072319_add_active_to_notices.rb
+++
@@ -1,5 +0,0 @@
-class AddActiveToNotices < ActiveRecord::Migration
-  def change
-    add_column :gubbara_notices, :active, :boolean, default: true
-  end
-end
