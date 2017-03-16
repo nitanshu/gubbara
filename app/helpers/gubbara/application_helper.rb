3631c5f8c41467c25794af06d35122fc4bb2883d
nitanshu verma <nitanshu1991@gmail.com>
2017-03-16 16:53:23 +0530

    changed the method name of list notice

--- app/helpers/gubbara/application_helper.rb
+++ app/helpers/gubbara/application_helper.rb
@@ -1,6 +1,6 @@
 module Gubbara
   module ApplicationHelper
-    def list_notice
+    def list_notices
       session[:hidden_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?) and gubbara_notices.active not in (?)", session[:hidden_notices], false) : @notices = Gubbara::Notice.all
       render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices})
     end
