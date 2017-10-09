12c6ea20f3d5307705e7724ed283e604dc0cfa19
nitanshu <niverma@q3tech.com>
2017-10-09 19:00:59 +0530

    readme updated according to introduction of cookie based announcement

--- app/helpers/gubbara/application_helper.rb
+++ app/helpers/gubbara/application_helper.rb
@@ -1,11 +1,11 @@
 module Gubbara
   module ApplicationHelper
-    def user_announcement
+    def user_announcements
       session[:hidden_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?) and gubbara_notices.active not in (?) and gubbara_notices.base not in (?)", session[:hidden_notices], false, 'cookie') : @notices = Gubbara::Notice.where("gubbara_notices.active not in (?) and gubbara_notices.base not in (?)", false, 'cookie')
       render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices})
     end
 
-    def public_announcement
+    def public_announcements
       cookies[:hidden_public_notices] ? @notices = Gubbara::Notice.where("gubbara_notices.id not in (?) and gubbara_notices.active not in (?) and gubbara_notices.base not in (?)", cookies[:hidden_public_notices].split, false, 'session') : @notices = Gubbara::Notice.where("gubbara_notices.active not in (?) and gubbara_notices.base not in (?)", false, 'session')
       render(:partial => 'gubbara/application/fetch_notices', locale: {notices: @notices})
     end
